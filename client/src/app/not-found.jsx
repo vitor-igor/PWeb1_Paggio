f9c959ba5431595aff31bb45c656a4df0db2e9ed
Kaeljane <kayyt0102@gmail.com>
2025-08-26 22:43:51 -0300

    Client: Atualizando Not-Found Page

--- client/src/app/not-found.jsx
+++ client/src/app/not-found.jsx
@@ -1,56 +1,26 @@
 import Link from "next/link";
+import { PiBookOpenBold } from "react-icons/pi";
 
 export default function NotFound() {
   return (
-    <>
-      {/* <h1>Parece que essa página não existe :/</h1>
-      <Link href="/">Voltar para Home</Link>
-       */}
-      <div style={{
-        background: "#1f182a",
-        color: "#fdf3f2",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        padding: "2rem"
-      }}>
-        {/* Ícone de livro */}
-        <div style={{
-          fontSize: "5rem",
-          marginBottom: "1rem"
-        }}>
-          📚 {/* Gostaria de colocar o icone do site */}
-        </div>
-        {/* Mensagem */}
-      <h1 style={{ fontSize: "2rem", marginBottom: "1rem" }}>
+    <div className="bg-preto-100 text-branco-100 flex min-h-[calc(100vh-64px)] flex-col items-center justify-center px-6 text-center">
+      <div className="border-roxo-100 mb-6 rounded-full border-6 p-4">
+        <PiBookOpenBold className="text-roxo-100 text-6xl" />
+      </div>
+
+      <h1 className="mb-3 text-2xl font-extrabold tracking-tight md:text-4xl">
         Ops! Página não encontrada...
       </h1>
-      <p style={{ maxWidth: "500px", marginBottom: "2rem" }}>
-        Parece que você se perdeu entre as páginas...  
-        Que tal voltar para a estante e escolher outro livro?
+      <p className="mb-6 max-w-lg text-sm text-zinc-300 md:text-base">
+        Parece que você se perdeu entre as páginas... Que tal voltar para a
+        estante e escolher outro livro?
       </p>
-      
-      {/* Botão de voltar */}
-      <Link href="/" style={{
-        backgroundColor: "#93256c",
-        color: "#fdf3f2",
-        padding: "0.8rem 1.5rem",
-        borderRadius: "8px",
-        textDecoration: "none",
-        fontWeight: "bold",
-        fontSize: "1rem",
-        transition: "background 0.3s"
-      }}
+
+      <Link
+        href="/"
+        className="bg-roxo-100 text-branco-100 rounded-xl px-6 py-2 font-bold shadow-lg transition-all hover:bg-[#a63a7e]"
       >
         Voltar para Home
       </Link>
-
-      </div>
-    </>
-  );
+</div>);
 }
-
-
