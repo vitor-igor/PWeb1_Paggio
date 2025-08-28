9b0536452d10c51c67f704b44dd3de51b9130ce2
Eduardo Brito Oliveira <eduardoerasla22@gmail.com>
2025-08-28 00:27:32 -0300

    Client: Atualizando Search

--- client/src/app/search/components/SearchBar.jsx
+++ client/src/app/search/components/SearchBar.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 
 export default function SearchBar({ onSearch }) {
+  const [search, setSearch] = useState("");
+
   return (
     <>
-      <div className="bar bg-branco-100 text-preto-100 rounded-full">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSearch(search);
+        }}
+        className="bar bg-branco-100 text-preto-100 rounded-full"
+      >
         <label className="flex justify-between p-1.5">
           <input
             type="text"
             placeholder="Pesquise pelo título, autor..."
             className="w-full pr-3 pl-3 outline-none"
-            onChange={(e) => onSearch(e.target.value)}
+            onChange={(e) => setSearch(e.target.value)}
           />
-          <div className="pr-2 pl-2">
+          <button type="submit" className="pr-2 pl-2">
             <IoSearchOutline className="h-full" />
-          </div>
+          </button>
         </label>
-      </div>
+      </form>
     </>
   );
 }
